25afb0b3dc1f7e3de55e0e2a60019d578bb4992c
Gia <gia.jihye.j@gmail.com>
2022-09-17 11:45:27 +0900

    react-router-dom 세팅중

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Home from "./pages/Home";
+import New from "./pages/New";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">hi</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
